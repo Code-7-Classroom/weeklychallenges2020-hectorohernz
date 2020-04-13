45ffa091717df53d060a633ac09e6494e4ed8006
Hector Hernandez <hectorohernz@gmail.com>
2020-04-13 19:47:53 -0400

    added final challenge of javascript week 4

--- javascript-week4/hard-week4/index.js
+++ javascript-week4/hard-week4/index.js
@@ -14,6 +14,33 @@ Example 2:
 Input: coins = [2], amount = 3
 Output: -1
 */
-
-
-// PSUDEO CODE
+function coinSort(coin, amount){
+    var finalResult = FindMinCount(coin, 0, 0, amount, 0, amount + 1);
+    function FindMinCount(coin, i, currentSum, amount, count, result){
+      if(amount == 0){
+        return 0;
+      } 
+      if(currentSum > amount){
+        return result;
+      }
+      if(currentSum == amount){
+        result = Math.min(count, result);
+        return result;
+      }
+      if(i <= coin.length - 1){
+        count++;
+        result = FindMinCount(coin, i, currentSum + coin[i], amount, count, result);
+  
+        count--;
+        result = FindMinCount(coin, i + 1, currentSum, amount, count, result);
+      }
+    return result;
+    }
+    if(finalResult == coin){
+      return -1; 
+    } else{
+      return finalResult; 
+    }
+  }
+console.log(coinSort([1,2,5],11));
+console.log(coinSort([2],3));
